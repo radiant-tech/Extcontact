7496ec345c8ea24e6addf9c0fb5f94f361a24f01
radiant-tech <denise@radiantconsultinginc.com>
2014-02-14 11:39:59 -0500

    Added sortname fields to VCF view and corrected Notice errors

--- com_extcontact/site/views/contact/view.vcf.php
+++ com_extcontact/site/views/contact/view.vcf.php
@@ -17,8 +17,10 @@ class ExtcontactViewContact extends JViewLegacy
 
 	protected $item;
 
-	public function display()
+	public function display($tpl = null)
 	{
+		$app = JFactory::getApplication();
+		
 		// Get model data.
 		$item = $this->get('Item');
 
@@ -31,37 +33,60 @@ class ExtcontactViewContact extends JViewLegacy
 
 		JFactory::getDocument()->setMetaData('Content-Type', 'text/directory', true);
 
-		// Compute lastname, firstname and middlename
+		// Get the parameters
+		$params = JComponentHelper::getParams('com_extcontact');
+		// Merge the item parameters
+		$params->merge($item->params);
+
 		$item->name = trim($item->name);
 
-		// "Lastname, Firstname Midlename" format support
-		// e.g. "de Gaulle, Charles"
-		$namearray = explode(',', $item->name);
-		if (count($namearray) > 1 )
+		// Use sortname fields if present for lastname, firstname and middlename
+		// Otherwise use older sorting method
+		if (!empty($item->sortname1) && !empty($item->sortname2))
 		{
-			$lastname = $namearray[0];
-			$card_name = $lastname;
-			$name_and_midname = trim($namearray[1]);
-
-			$firstname = '';
-			if (!empty($name_and_midname))
+			$lastname = (string) $item->sortname1;
+			$firstname = (string) $item->sortname2;
+			$middlename = (!empty($item->sortname3) ? (string) $item->sortname3 : '');
+			$card_name = $firstname . ($middlename ? ' ' . $middlename : '') . ' ' . $lastname;
+		}
+		else 
+		{
+			// "Lastname, Firstname Midlename" format support
+			// e.g. "de Gaulle, Charles"
+			$namearray = explode(',', $item->name);
+			if (count($namearray) > 1 )
 			{
-				$namearray = explode(' ', $name_and_midname);
-
-				$firstname = $namearray[0];
-				$middlename = (count($namearray) > 1) ? $namearray[1] : '';
-				$card_name = $firstname . ' ' . ($middlename ? $middlename . ' ' : '') .  $card_name;
+				$lastname = $namearray[0];
+				$card_name = $lastname;
+				$name_and_midname = trim($namearray[1]);
+			
+				$firstname = '';
+				if (!empty($name_and_midname))
+				{
+					$namearray = explode(' ', $name_and_midname);
+			
+					$firstname = $namearray[0];
+					$middlename = (count($namearray) > 1) ? $namearray[1] : '';
+					$card_name = $firstname . ' ' . ($middlename ? $middlename . ' ' : '') .  $card_name;
+				}
+			}
+			// "Firstname Middlename Lastname" format support
+			else {
+				$namearray = explode(' ', $item->name);
+			
+				$middlename = (count($namearray) > 2) ? $namearray[1] : '';
+				$firstname = array_shift($namearray);
+				$lastname = count($namearray) ? end($namearray) : '';
+				$card_name = $firstname . ($middlename ? ' ' . $middlename : '') . ($lastname ? ' ' . $lastname : '');
 			}
 		}
-		// "Firstname Middlename Lastname" format support
-		else {
-			$namearray = explode(' ', $item->name);
-
-			$middlename = (count($namearray) > 2) ? $namearray[1] : '';
-			$firstname = array_shift($namearray);
-			$lastname = count($namearray) ? end($namearray) : '';
-			$card_name = $firstname . ($middlename ? ' ' . $middlename : '') . ($lastname ? ' ' . $lastname : '');
-		}
+		
+		// Set up address fields
+		$address	= (!empty($item->address) ? $item->address : $params->get('default_street_address', ''));
+		$suburb		= (!empty($item->suburb) ? $item->suburb : $params->get('default_suburb', ''));
+		$state		= (!empty($item->state) ? $item->state : $params->get('default_state', ''));
+		$postcode	= (!empty($item->postcode) ? $item->postcode : $params->get('default_postcode', ''));
+		$country	= (!empty($item->country) ? $item->country : ''); 
 
 		$rev = date('c', strtotime($item->modified));
 
@@ -76,8 +101,8 @@ class ExtcontactViewContact extends JViewLegacy
 		$vcard[]  = 'TEL;TYPE=WORK,VOICE:'.$item->telephone;
 		$vcard[]  = 'TEL;TYPE=WORK,FAX:'.$item->fax;
 		$vcard[]  = 'TEL;TYPE=WORK,MOBILE:'.$item->mobile;
-		$vcard[]  = 'ADR;TYPE=WORK:;;'.$item->address.';'.$item->suburb.';'.$item->state.';'.$item->postcode.';'.$item->country;
-		$vcard[]  = 'LABEL;TYPE=WORK:'.$item->address."\n".$item->suburb."\n".$item->state."\n".$item->postcode."\n".$item->country;
+		$vcard[]  = 'ADR;TYPE=WORK:;;'.$address.';'.$suburb.';'.$state.';'.$postcode.';'.$country;
+		$vcard[]  = 'LABEL;TYPE=WORK:'.$address."\n".$suburb."\n".$state."\n".$postcode."\n".$country;
 		$vcard[]  = 'EMAIL;TYPE=PREF,INTERNET:'.$item->email_to;
 		$vcard[]  = 'URL:'.$item->webpage;
 		$vcard[]  = 'REV:'.$rev.'Z';
